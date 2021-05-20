9d40135bd55996e92b72e250c8486c5791896e79
michal4511 <fuliermichal96@gmail.com>
2021-05-20 21:07:31 +0100

    connection between the shopping list and basket

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const addButton = document.getElementById("add-button");
 const productInput = document.getElementById("product-input");
 const headingTitle = document.getElementById("heading-tertiary");
-
+const priceInput = document.getElementById("price-input");
+const basketList = document.getElementById("basket-list");
 //Event Listener
 addButton.addEventListener("click", checkInputField);
 
@@ -19,9 +20,20 @@ function checkInputField(event) {
 function getProductToBasket() {
     //Remove heading <h3> in a basket because of a list of products
     headingTitle.classList.add("hide");
-        //Product Div
-        const productDiv = document.createElement("div");
-        productDiv.classList.add("product-in-basket")
-    
+    //Product Div
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product-in-basket")
+    //Create LI for product
+    const newProduct = document.createElement("li");
+    newProduct.innerText = productInput.value;
+    newProduct.classList.add("product-item");
+    productDiv.appendChild(newProduct);
+    //Create LI for price
+    const newPrice = document.createElement("li");
+    newPrice.classList.add("price-item");
+    newPrice.innerText = priceInput.value;
+    productDiv.appendChild(newPrice);
+    //Append to basket list
+    basketList.appendChild(productDiv);
 
 }
